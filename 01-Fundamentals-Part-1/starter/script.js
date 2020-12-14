9cf2d686fc7ff9f0f210b9e116ad1e75c3abf678
Ryan <ryanmahara@gmail.com>
2020-12-14 07:00:39 -0600

    on section 13 next, still need assignments done

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,7 +1,26 @@
-let js = "amazing";
+/*let js = "amazing";
 console.log(40 + 8 + 23 - 10);
 
 console.log(23);
 
+// this is a comment
 let firstname = "jonas"
 console.log(firstname);
+*/
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun)
+
+
+// console.log(typeof true);
+console.log(typeof javascriptIsFun);
+// console.log(typeof 31);
+
+javascriptIsFun = 'YES!';
+console.log(javascriptIsFun)
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+console.log(typeof null);
