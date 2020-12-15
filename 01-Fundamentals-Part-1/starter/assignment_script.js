8155c98ae52cf67fd43879fa90ee6fc79678dde3
Ryan <ryanmahara@gmail.com>
2020-12-15 05:46:03 -0600

    added assignment files and did first 2 assigments

---
+++ 01-Fundamentals-Part-1/starter/assignment_script.js
@@ -0,0 +1,18 @@
+
+// Lecture Assignment: Values and Variables
+let country = "USA";
+let continent = "North America";
+let population = 328.2;
+
+console.log(country)
+console.log(continent)
+console.log(population)
+
+// Lecture Assignment: Data Types
+let isIsland = true;
+let language
+
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
