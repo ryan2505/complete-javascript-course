7620761d99728d8edb99c3944068bfded95a7295
Ryan <ryanmahara@gmail.com>
2020-12-15 07:23:59 -0600

    on section 16 now, all assignments done up to now

--- 01-Fundamentals-Part-1/starter/assignment_script.js
+++ 01-Fundamentals-Part-1/starter/assignment_script.js
@@ -1,7 +1,8 @@
 
 // Lecture Assignment: Values and Variables
-let country = "USA";
-let continent = "North America";
+/*
+const country = "USA";
+const continent = "North America";
 let population = 328.2;
 
 console.log(country)
@@ -9,10 +10,29 @@ console.log(continent)
 console.log(population)
 
 // Lecture Assignment: Data Types
-let isIsland = true;
+const isIsland = true;
 let language
 
 console.log(typeof isIsland);
 console.log(typeof population);
 console.log(typeof country);
 console.log(typeof language);
+
+// Lecture Assignment: let, const and var
+
+language = "English"
+
+// Lecture Assignment: Basic Operators
+
+let populationHalf = population / 2;
+population++;
+console.log(population); // 329.2
+
+let finlandPopulation = 6;
+console.log(finlandPopulation > population); // false
+
+const avgCountryPopulation = 33;
+console.log(population < avgCountryPopulation);
+
+console.log(country + " is in " + continent + " ,and its " + population +" million people speak " + language);
+*/ 
--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -6,7 +6,7 @@ console.log(23);
 // this is a comment
 let firstname = "jonas"
 console.log(firstname);
-*/
+
 
 let javascriptIsFun = true;
 console.log(javascriptIsFun)
@@ -24,3 +24,51 @@ console.log(year);
 console.log(typeof year);
 
 console.log(typeof null);
+
+
+let age = 30;
+
+
+const birthYear = 1991;
+
+var job = 'programmer';
+job = 'teacher';
+
+// math operators
+const currentYear = 2037;
+const ageJones = currentYear - 1991;
+const ageSarah = currentYear - 2018;
+console.log((currentYear - 1991), ageSarah);
+
+console.log(ageJones * 2, ageJones / 10, 2 ** 3);
+// 2 ** 3 means 2 to the power of 3 = 2*2*2
+
+const firstName = 'Ryan';
+const lastName = 'mahara';
+console.log(firstName+" "+ lastName);
+
+// assignment operators 
+let x = 10 + 5; // 15
+x += 10; //x = x + 10 = 25
+x *= 4; // x = x * 4 = 100
+x++; // x = x + 1 = 101
+
+console.log(x);
+
+// comparison operators;
+console.log(ageJones > ageSarah);
+console.log(ageSarah >= 18);
+*/
+
+const currentYear = 2037;
+const ageJones = currentYear - 1991;
+const ageSarah = currentYear - 2018;
+
+console.log(currentYear - 1991 > currentYear - 2018);
+
+let x, y;
+x = y = 25-10-5; // x = y = 10
+console.log(x, y);
+
+const averageAge = (ageJones + ageSarah) / 2;
+console.log(ageJones, ageSarah, averageAge);
