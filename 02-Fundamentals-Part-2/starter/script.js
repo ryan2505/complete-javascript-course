3c57470157ac530ca5fdc6a70c6e073a85837e94
Ryan <ryanmahara@gmail.com>
2020-12-23 06:57:57 -0600

    on section 34

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,7 +1,33 @@
 "use strict";
+/*
 
 let hasDriversLicense = false;
 const passTest = true;
 
 if (passTest) hasDriversLicense = true;
-if (hasDriversLicense) console.log("I can drive :D");
+if (hasDrigersLicense) console.log("I can drive :D");
+*/
+
+function logger() {
+  console.log("My name is jonas");
+}
+
+logger();
+
+function fruitProcessor(apples, oranges) {
+  console.log(apples, oranges);
+  const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+  return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
+
+function adder(x, y) {
+  return x + y;
+}
+
+console.log(adder(2, 3) + 3);
