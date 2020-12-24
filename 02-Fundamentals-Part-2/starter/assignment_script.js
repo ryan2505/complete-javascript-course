f5fc2df7aa118214c9f6e3025cef33a8d921df3d
Ryan <ryanmahara@gmail.com>
2020-12-24 13:39:47 -0600

    on section 39, up to date on assignments and challenges

---
+++ 02-Fundamentals-Part-2/starter/assignment_script.js
@@ -0,0 +1,53 @@
+"use strict";
+/*
+// LECTURE: Functions
+
+function describeCountry(country, population, capitalCity) {
+  console.log(`${country} has ${population} million people and it capital city is ${capitalCity}`);
+}
+
+describeCountry("Argentina", 45, "Buenos Aires");
+describeCountry("Greece", 10, "Athens");
+describeCountry("Italy", 60, "Rome");
+
+//LECTURE: Function Declarations vs. Expressions
+
+function percentageOfWorld1(population) {
+  // world population is 7.594 billion as of 2018
+  return (population / 7594) * 100;
+}
+
+const argentinapopulation = percentageOfWorld1(45);
+const greecepopulation = percentageOfWorld1(10);
+const italypopulation = percentageOfWorld1(60);
+
+console.log(`${argentinapopulation}%`);
+console.log(`${greecepopulation}%`);
+console.log(`${italypopulation}%`);
+
+// below is the same as above but as function expressions
+
+const percentageOfWorld2 = function (population) {
+  // world population is 7.594 billion as of 2018
+  return (population / 7594) * 100;
+};
+
+const argentinapopulation2 = percentageOfWorld2(45);
+const greecepopulation2 = percentageOfWorld2(10);
+const italypopulation2 = percentageOfWorld2(60);
+
+console.log(`${argentinapopulation2}%`);
+console.log(`${greecepopulation2}%`);
+console.log(`${italypopulation2}%`);
+
+
+const percentageOfWorld3 = (population) => (population / 7594) * 100;
+
+const argentinapopulation3 = percentageOfWorld3(45);
+const greecepopulation3 = percentageOfWorld3(10);
+const italypopulation3 = percentageOfWorld3(60);
+
+console.log(`${argentinapopulation3}%`);
+console.log(`${greecepopulation3}%`);
+console.log(`${italypopulation3}%`);
+*/
--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -6,7 +6,7 @@ const passTest = true;
 
 if (passTest) hasDriversLicense = true;
 if (hasDrigersLicense) console.log("I can drive :D");
-*/
+
 
 function logger() {
   console.log("My name is jonas");
@@ -31,3 +31,67 @@ function adder(x, y) {
 }
 
 console.log(adder(2, 3) + 3);
+
+
+//function declaration
+function calcAge1(birthYear) {
+  return 2037 - birthYear;
+}
+
+console.log(calcAge1(2020));
+
+//function expression
+const calcAge2 = function (birthYear) {
+  return 2037 - birthYear;
+};
+
+console.log(calcAge2(2023));
+
+
+// arrow function
+
+const calcAge3 = (birthYear) => 2037 - birthYear;
+
+console.log(calcAge3(2020));
+
+const yearsUntilRetirement = (birthYear, firstname) => {
+  const age = 2037 - birthYear;
+  const retirement = 65 - age;
+  //return retirement;
+  return `${firstname} retires in ${retirement} years`;
+};
+
+console.log(yearsUntilRetirement(1997, "ryan"));
+
+
+function cutFruitPieces(fruit) {
+  return fruit * 4;
+}
+
+function fruitProcessor(apples, oranges) {
+  const applePieces = cutFruitPieces(apples);
+  const orangePieces = cutFruitPieces(oranges);
+
+  const juice = `Juice with ${applePieces} apples and ${orangePieces} oranges.`;
+  return juice;
+}
+
+console.log(fruitProcessor(2, 3));
+
+
+const calcAge = function (birthYear) {
+  return 2037 - birthYear;
+};
+
+const yearsUntilRetirement = function (birthYear, firstname) {
+  const retirement = 65 - calcAge(birthYear);
+  if (retirement > 0) {
+    return retirement;
+  } else {
+    return -1;
+  }
+};
+
+console.log(yearsUntilRetirement(1991, "jonas"));
+console.log(yearsUntilRetirement(1950, "Mike"));
+*/
