426918dd621bc3f1ca3738149b5a6f34d3401860
Ryan <ryanmahara@gmail.com>
2020-12-27 17:02:41 -0600

    on section 40

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -95,3 +95,36 @@ const yearsUntilRetirement = function (birthYear, firstname) {
 console.log(yearsUntilRetirement(1991, "jonas"));
 console.log(yearsUntilRetirement(1950, "Mike"));
 */
+
+const friends = ["Michael", "Steven", "Peter"];
+console.log(friends);
+
+//const years = new Array(1991, 1984, 2008, 2020);
+
+console.log(friends[0]);
+console.log(friends[2]);
+
+console.log(friends.length);
+console.log(friends[friends.length - 1]);
+
+friends[2] = "Jay";
+console.log(friends);
+
+const jonas = ["Jonas", "Schmedtmann", 2037 - 1991, "teacher", friends];
+console.log(jonas);
+
+//exercise
+function calcAge(birthYear) {
+  return 2037 - birthYear;
+}
+
+const years = [1990, 1967, 2002, 2010, 2018];
+
+const age1 = calcAge(years[0]);
+const age2 = calcAge(years[1]);
+const age3 = calcAge(years[3]);
+
+console.log(age1, age2, age3);
+
+const ages = [calcAge(years[0]), calcAge(years[1]), calcAge(years[years.length - 1])];
+console.log(ages);
