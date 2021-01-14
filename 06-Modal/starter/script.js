33b34cb7efc2ac39c95c04fc9b2a9a0327f4ae1b
Ryan <ryanmahara@gmail.com>
2021-01-14 06:55:35 -0600

    on section 81

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -5,6 +5,20 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsShowModal = document.querySelectorAll('.show-modal');
 
-for (let i = 0; i < btnsShowModal.length; i++) {
-  console.log(btnsShowModal[i].textContent);
+function openModel() {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
 }
+
+function closeModel() {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+}
+
+for (let i = 0; i < btnsShowModal.length; i++) {
+  btnsShowModal[i].addEventListener('click', openModel);
+}
+
+btnCloseModal.addEventListener('click', closeModel);
+
+overlay.addEventListener('click', closeModel);
