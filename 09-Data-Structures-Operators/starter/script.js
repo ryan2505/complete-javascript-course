017e2514f888508b3de58cc7de282fa95c8c0487
Ryan <ryanmahara@gmail.com>
2021-03-03 06:42:03 -0600

    finishing coding challenge 4, on section 124

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -52,6 +52,167 @@ const restaurant = {
   },
 };
 
+///////////////////////////////////////
+// String Methods Practice
+const flights =
+  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+// 🔴 Delayed Departure from FAO to TXL (11h25)
+//              Arrival from BRU to FAO (11h45)
+//   🔴 Delayed Arrival from HEL to FAO (12h05)
+//            Departure from FAO to LIS (12h30)
+
+/*
+//////sting methods
+
+//split and join
+console.log('a+very+nice+string'.split('+'));
+console.log('ryan mahara'.split(' '));
+
+const [firstname, lastname] = 'ryan mahara'.split(' ');
+console.log(firstname, lastname);
+
+const newName = ['Mr.', firstname, lastname.toUpperCase()].join(' ');
+console.log(newName);
+
+function capitalizeName(name) {
+  const names = name.split(' ');
+  const namesUpper = [];
+  for (const n of names) {
+    namesUpper.push(n[0].toUpperCase() + n.slice(1));
+  }
+
+  console.log(namesUpper.join(' '));
+}
+
+capitalizeName('jessica ann smith davis');
+capitalizeName('ryan mahara');
+
+// padding a string
+
+const message = 'Go to gate 23!';
+console.log(message.padStart(25, '+').padEnd(30, '+'));
+
+// real example
+
+function maskCreditCard(number) {
+  const str = number + '';
+  const last = str.slice(-4);
+  return last.padStart(str.length, '*');
+}
+
+console.log(maskCreditCard(43378463864647384));
+console.log(maskCreditCard('65156484984984984'));
+
+//Repeat
+const message2 = 'Bad weather... All Departues Delayed... ';
+
+console.log(message2.repeat(5));
+
+const planesInLine = function (n) {};
+
+console.log(planesInLine(5));
+*/
+/*
+////////////////////working with strings - part 2
+const airline = 'TAP Air Portugal';
+
+console.log(airline.toLowerCase());
+console.log(airline.toUpperCase());
+
+//Fix capitalization in name
+const passenger = 'jOnAs'; //Jonas
+const passengerLower = passenger.toLowerCase();
+const passengerCorrect =
+  passengerLower[0].toUpperCase() + passengerLower.slice(1);
+console.log(passengerCorrect);
+
+//Comparing email
+const email = 'hello@jonas.io';
+const loginEmail = ' Hello@Jonas.Io \n';
+
+// const lowerEmail = loginEmail.toLowerCase();
+// const trimmedEmail = lowerEmail.trim();
+
+const normalizedEmail = loginEmail.toLowerCase().trim();
+console.log(normalizedEmail);
+console.log(email === normalizedEmail);
+
+// replacing
+
+const priceGB = '288,97£'; //Great britain price
+const priceUS = priceGB.replace('£', '$').replace(',', '.');
+console.log(priceUS);
+
+const announcement =
+  'All passengers come to borading door 23. Boarding door 23!';
+// console.log(announcement.replaceAll('door', 'gate')); // ecma 2021
+
+console.log(announcement.replace(/door/g, 'gate'));
+
+//Booleans
+const plane = 'Airbus A320neo';
+console.log(plane.includes('A320'));
+console.log(plane.startsWith('A320'));
+console.log(plane.endsWith('neo'));
+
+if (plane.startsWith('Airbus') && plane.endsWith('neo')) {
+  console.log('Part of the NEW Airbus family');
+}
+
+// practice exercise
+
+const checkBaggage = function (items) {
+  const baggage = items.toLowerCase();
+
+  if (baggage.includes('knife') || baggage.includes('gun')) {
+    console.log(`You are not allowed on board`);
+  } else {
+    console.log(`Welcome aboard!`);
+  }
+};
+
+checkBaggage('I have a laptop, some Food and a Pocket Knife');
+checkBaggage('Socks and camera');
+checkBaggage('Got some snacks and a gun for protection');
+*/
+/*
+///////////////string methods
+const plane = 'A320';
+
+console.log(...plane);
+console.log('B737'[0]);
+console.log(airline.length);
+
+console.log(airline.indexOf('r'));
+console.log(airline.lastIndexOf('r'));
+console.log(airline.indexOf('Portugal'));
+
+console.log(airline.slice(4));
+console.log(airline.slice(4, 7));
+
+console.log(airline.slice(0, airline.indexOf(' ')));
+console.log(airline.slice(airline.lastIndexOf(' ') + 1));
+
+console.log(airline.slice(-2));
+console.log(airline.slice(1, -1));
+
+const checkMiddle = function (seat) {
+  //B and E are middle seats
+  const s = seat.slice(-1);
+  if (s === 'B' || s === 'E') {
+    console.log('You got the middle seat 😭');
+  } else {
+    console.log('You got lucky');
+  }
+};
+
+checkMiddle('11B');
+checkMiddle('23C');
+checkMiddle('3E');
+
+console.log(new String('jonas'));
+
+*/
 /*
 //////////////////////convert objects to maps data structure
 const question = new Map([
