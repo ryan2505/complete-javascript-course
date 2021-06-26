b90528d906d368e7700818d07a1efeae07c89c7b
Ryan <ryanmahara@gmail.com>
2021-06-26 12:37:54 -0500

    finished section 17, removed all final folders

--- 17-Modern-JS-Modules-Tooling/starter/clean.js
+++ 17-Modern-JS-Modules-Tooling/starter/clean.js
@@ -1,45 +1,117 @@
-var sc = [
-  { product: 'bread', quantity: 6 },
-  { product: 'pizza', quantity: 2 },
-  { product: 'milk', quantity: 4 },
-  { product: 'water', quantity: 10 },
-];
+'use strict';
 
-var allow = {
-  lisbon: 5,
-  others: 7,
+// Object.freeze makes the object immutable (cant add to)
+const budget = Object.freeze([
+  { value: 250, description: 'Sold old TV 📺', user: 'jonas' },
+  { value: -45, description: 'Groceries 🥑', user: 'jonas' },
+  { value: 3500, description: 'Monthly salary 👩‍💻', user: 'jonas' },
+  { value: 300, description: 'Freelancing 👩‍💻', user: 'jonas' },
+  { value: -1100, description: 'New iPhone 📱', user: 'jonas' },
+  { value: -20, description: 'Candy 🍭', user: 'matilda' },
+  { value: -125, description: 'Toys 🚂', user: 'matilda' },
+  { value: -1800, description: 'New Laptop 💻', user: 'jonas' },
+]);
+
+// freeze can't stop this, it only stops new elements being added
+// a 3rd party library could be used to make a deep freeze where this wouldn't work
+// budget[0].value = 100000;
+
+// Object.freeze makes the object immutable (cant add to)
+// only freezes first level of the object not a Deep freeze
+const spendingLimits = Object.freeze({
+  jonas: 1500,
+  matilda: 100,
+});
+
+// const limit = spendingLimits[user] ? spendingLimits[user] : 0;
+// same thing - ES6+
+// old way used a variable outside its scope which is bad practice of functional programming
+// const getLimit = (user) => spendingLimits?.[user] ?? 0;
+// new version we pass in spendingLimits object to use, no longer has a side affect
+const getLimit = (limits, user) => limits?.[user] ?? 0;
+
+// here we pass in 5 arguments but we ideally should stick to 3 max.
+// to fix this we could pass an object of options but wont for this example
+// Pure Function :D
+const addExpense = function (
+  state,
+  limits,
+  value,
+  description,
+  user = 'jonas'
+) {
+  // convert to lowercase required to access object property
+  // new variable instead of mutating user pass in
+  const cleanUser = user.toLowerCase();
+
+  return value <= getLimit(limits, cleanUser)
+    ? // im property name is the same as setting name we dont need description: description
+      [...state, { value: -value, description, user: cleanUser }]
+    : state; // return original state if price over limit
+  // avoid mutating the budget array
+  // budget.push({ value: -value, description, user: cleanUser });
 };
 
-var description = '';
+// by setting each to a different variables we retain the same array as we add more
+// before when we had just budget it was adding to the array but didn't contain the other adds
 
-var check = function (city) {
-  if (sc.length > 0) {
-    var allowed;
-    if (city == 'lisbon') {
-      allowed = allow.lisbon;
-    } else {
-      allowed = allow.others;
-    }
+// in real world we would use something called composing to create one function to preform all these chains at once
+// google composing functions
+// this is important for frameworks like react
+const newBudget1 = addExpense(budget, spendingLimits, 10, 'Pizza 🍕');
+const newBudget2 = addExpense(
+  newBudget1,
+  spendingLimits,
+  100,
+  'Going to movies 🍿',
+  'Matilda'
+);
+const newBudget3 = addExpense(newBudget2, spendingLimits, 200, 'Stuff', 'Jay');
 
-    for (item of sc) {
-      if (item.quantity > allowed) item.quantity = allowed;
-    }
-  }
+console.log(newBudget3);
+
+// pure function now
+const checkExpenses = function (state, limits) {
+  // return brand new array
+  return state.map(entry => {
+    // need to return in map to keep going
+    return entry.value < -getLimit(limits, entry.user)
+      ? { ...entry, flag: 'limit' } // add flag property to object that meets condition
+      : entry; // return original object if no changes required
+  });
+
+  // old way - mutates original budget array
+  // for (const entry of budget) {
+  //   if (entry.value < -getLimit(limits, entry.user)) {
+  //     entry.flag = 'limit';
+  //   }
+  // }
 };
-check('lisbon');
-console.log(sc);
 
-var createDescription = function () {
-  var first = sc[0];
-  var p = first.product;
-  var q = first.quantity;
+const finalBudget = checkExpenses(newBudget3, spendingLimits);
 
-  if (sc.length > 1) {
-    description = 'Order with ' + q + ' ' + p + ', etc...';
-  } else {
-    description = 'Order with ' + q + ' ' + p + '.';
-  }
+console.log(finalBudget);
+
+// impure function because it creates a side effect which is the console.log()
+// because the console.log effects the console
+const logBigExpenses = function (state, bigLimit) {
+  // new functional version - doesn't rely on outside variables
+  const bigExpenses = state
+    .filter(entry => entry.value <= -bigLimit)
+    .map(entry => entry.description.slice(-2))
+    .join(' / ');
+
+  // makes the function impure
+  console.log(bigExpenses);
+  // old way mutates the output variable on each loop that passes the condition
+  // let output = '';
+  // for (const entry of budget) {
+  //   // more declarative
+  //   output +=
+  //     entry.value <= -bigLimit ? `${entry.description.slice(-2)} / ` : ''; // Emojis are 2 chars
+  // }
+  // output = output.slice(0, -2); // Remove last '/ '
+  // console.log(output);
 };
-createDescription();
 
-console.log(description);
+logBigExpenses(finalBudget, 100);
--- 17-Modern-JS-Modules-Tooling/starter/script.js
+++ 17-Modern-JS-Modules-Tooling/starter/script.js
@@ -1,5 +1,5 @@
 // Importing module
-/*
+
 // when importaning the .js at the end isn't required
 // all the code in the import is parsed and executed first
 // import statements are hoisted to the top
@@ -19,41 +19,34 @@
 // import * as ShoppingCart from "./shoppingCart.js";
 // this is almost like if we made a class instance
 
-
 // ShoppingCart.addToCart('bread', 5)
 // console.log(ShoppingCart)
 // console.log(ShoppingCart.totalPrice)
-
 
 // we can mix default and named imports in the same line
 // in practice we don't do this because the perfered style is to import one default value per module if using default
 // import add, { addToCart, totalPrice as price, qt }  from './shoppingCart.js'
 
 // import default
-import add, {cart} from './shoppingCart.js'
+import add, { cart } from './shoppingCart.js';
 
-
-
-add('egg', 10)
-add('bread', 2)
-add('pizza', 5)
-
+add('egg', 10);
+add('bread', 2);
+add('pizza', 5);
 
 // proving modules are live connection and not copies
 // cart in shoppingcart.js is empty
 // above we added (3) items to the cart using addToCart() function
 // live connection means this cart puts to the same thing in memory and is not a copy
-console.log(cart)
+console.log(cart);
 
-// will print 
+// will print
 //(3) [{…}, {…}, {…}]
 // 0: {product: "egg", quantity: 10}
 // 1: {product: "bread", quantity: 2}
 // 2: {product: "pizza", quantity: 5}
 // length: 3
 
-
-*/
 /*
 ////////////////////////////
 // module pattern - old way to do modules
@@ -135,7 +128,6 @@ const { addToCart } = require('./shoppingCart.js')
 // end goal - ES6 modules should replace all modules but thats far away
 */
 
-
 ////////////////////////////
 // Introduction to node JS
 ////////////////////////////
@@ -146,7 +138,6 @@ const { addToCart } = require('./shoppingCart.js')
 // if we downloaded the API library, we would have to manual updated everytime the API changed
 // before NPM there used to be multiple places you could get your packages leading to more confusion
 // all of this made a big mess
-
 
 // Intro to NPM
 // to use NPM we need node.js installed as its part of it
@@ -183,26 +174,175 @@ const { addToCart } = require('./shoppingCart.js')
 // the package.json files keeps track of all your dependencies so even when you delete the node-modules folder you can get it back
 // use the command "npm i" with no package filled out will download them all again from package.json
 
-
 // to import cloneDeep.js within lodash
 
-import cloneDeep from './node_modules/lodash-es/cloneDeep.js'
+// import cloneDeep from './node_modules/lodash-es/cloneDeep.js';
+
+// with parcel it will read this import and automatically find this clonedeep function its self
+// it doesn't need the full address like above
+// this works for all project addresses like img, html, css, modules
+import cloneDeep from 'lodash-es';
+
+// if we import a package we don't have downloaded, parcel will read that and run "npm i lodash" to install it and use it
+// parcel works with CommonJS modules
+// import cloneDeep from 'lodash';
 
 const state = {
-    cart: [
-        {product: 'bread', quantity: 5},
-        {product: 'pizza', quantity: 5}
-    ],
-    user: {loggedIn: true}
-}
+  cart: [
+    { product: 'bread', quantity: 5 },
+    { product: 'pizza', quantity: 5 },
+  ],
+  user: { loggedIn: true },
+};
 
-const stateClone = Object.assign({}, state)
+const stateClone = Object.assign({}, state);
 
 // this is a clone not a deep copy
-console.log(stateClone)
+console.log(stateClone);
 
-const stateDeepClone = cloneDeep(state)
-console.log(stateDeepClone)
+const stateDeepClone = cloneDeep(state);
+console.log(stateDeepClone);
 
 // will change stateClone too
-state.user.loggedIn = false
+state.user.loggedIn = false;
+
+////////////////////////////
+// Bundling with parcel and NPM scripts
+////////////////////////////
+
+// parcel works out of the box without any configuration
+// webpack is really popular too for react world, but very complicated with config setups
+
+// Parcel is a build too on NPM
+// too install we write "npm i parcel --save-dev"
+// this is a different type of dependency, a dev dependency is a tool we need to build our project/app but not required in the code
+// dev dependencies are in a different section in the package.json file
+// parcel doesn't work with locally installed dependency's and with the above command we installed it locally
+// so its only within this project
+// to use parcel in the terminal we can use npx or npm scripts
+// first thing we will try is npx which is built into npm
+// "npx parcel index.html" <----- index.html is the entry point where we included our script.js src (the file we want to bundle)
+// currently in this example script we are importing shoppingcart.js, using lodash cloneDeep, and useing it all in script.js
+// so parcel should bundle these (3) things for us
+// when we run this "npx parcel index.html" in the terminal. parcel bundles are packages and gives us a new server instance
+// Server running at http://localhost:1234 is now runnning our code with the bundled stuff
+// parcel does the same thing live server does basically, throws the website on localhost aka http://127.0.0.1/
+// a new JS file is created with parcel code injected and all our code modified to use in one file.
+// our 200 line script becomes 3340 lines with parcel stuff and other packages like lodash code required
+
+// if parcel doesn't install we could use "sudo" in the command to give more admin rights
+// or install the exact version we know it works by typing "npm i parcel@1.12.4" to get a version we want
+// we might get errors because we defined a "type = module" script for our ES6 module to work but parcel builds a new script
+// and doesn't need it so we comment it out and create a version without "type = module" after saving parcel will rebuild
+// the application
+// each file save, parcel rebuilds
+
+// what parcel did:
+// created a "dist" folder standing for distribution, this is the folder we will send to production
+// this code we will send to finally users
+// in the "dist" folder we can see all the files parcel created
+// there will be a new index.html, JS script, and since we use leaflet a new map js file
+// JS file called "index.b4f5078c.js"
+// inside this file we can find uncompressed JS code with our code mixed in and modified but variable names stay the same
+// this is called the development step, compression and tree shaking out old/unused code comes later in the build step
+// this bundled super script, we normally don't care about in development. we keep modifying and fixing our original scripts
+// and modules then let parcel figure out the bundling
+
+// every save parcel rebuilds but in parcel we can activate hot module replacement
+// this code below is how we activate it. this code only parcel understands so it is instructions for parcel that wont make it into
+// the final bundle
+
+// hot module reloading =  when we modify one of the modules that will get put in parcel rebuild but won't force the page to reload
+// it will magically get injected into the script in the page so if we are testing any state type components they won't restart
+
+// if (module.hot) {
+//   module.hot.accept();
+// }
+
+// the other way to use parcel is to use npm scripts which is how we actually use it in practice/ real world
+// npm scripts let us bundle local scripts and automate tasks like commands we reuse over and over
+// in the terminal we cant write "parcel index.html" but in package.json script section we can write:
+// "scripts": {"start": "parcel index.html"}
+// and use it in the terminal with "npm run start" <--- start is the name of the NPM script we created/defined
+
+// When were done developing our project we need to build the final bundle
+// final bundle is compressed, with unused code eliminated
+// for this we need another parcel command that we create in the package.json scripts section
+// "build": "parcel build index.html" to run in the terminal with "npm run build"
+// this compresses all the files to save on space/ performance improved and puts them in "dist" folder
+// these files would be our production files
+
+// we can also install packages globally meaning we can use them in every directory on our computer
+// "npm i parcel -g" for that
+// most tools you should install locally so you stay on the latest version you need for the project
+
+////////////////////////////
+// Configuring Babel and polyfilling
+////////////////////////////
+
+// Babel is used for converting ES6+ code back to ES5
+// so our application can be used old versions of internet explorer
+// parcel includes babel with some default config decisions
+// Babel works with plugins and presets that can both be configured
+// a plugin is a specific JS feature we want to transpile (aka convert)
+// example: lets say we only want to convert arrow functions to ES5 but leave everything else in ES6 - so we would use this plugin
+// if we wanted to convert everything we would use a preset, which is a bunch of plugins bundled together.
+// by default parcel uses "@babel/preset-env for compiling ES2015+ syntax"
+// this preset automatically selects which JS features should be compiled/converted based browser support
+// "@babel/preset-env supports all browsers except browsers with less then 0.25% market usage
+
+// when we run "npm run start" ""start": "parcel index.html""
+// inside the JS script we can see all variables are in VAR and no longer LET or CONST
+// meaning we have reverted back to ES5
+// @babel/preset-env only includes final features of JS so all features passed stage 4 of the ecma process
+// a stage 3 feature wouldn't convert like class fields for private variables
+// this is how we would transpile that
+
+class Person {
+  greeting = 'Hey';
+  constructor(name) {
+    this.name = name;
+    console.log(`${this.greeting}, ${this.name}`);
+  }
+}
+
+const jonas = new Person('Jonas');
+
+// this works because the experimental JS plugin for babel is already inside the preset @babel/preset-env
+
+// below is an examples of using an ES6 features of filter and promise that won't convert to ES5
+// the below code will work for us because our browser understands ES6 but on internet explore 7 it wouldn't
+// we can find this not converted in the dev bundle file parcel makes
+
+console.log(cart.filter(el => el.quantity >= 2));
+Promise.resolve('TEST').then(x => console.log(x));
+
+// babel can only convert syntax only so arrow functions, spread operator do things in a shorten way
+// that babel can write the long way over to convert
+// but filter and promise are new JS features not syntax
+
+// for these features we need to polyfill them, meaning converting there purpose into the ES5 equivalent
+// babel used to do this but no recommends the core-js library
+// in terminal "npm i core-js" to install
+
+// import 'core-js/stable';
+
+// core-JS/stable is what we need for these features
+// what will happen now is Core-JS will recreate the filter and promise as functions that follow es5
+// so that can still be used
+
+// polyfill will polyfill everything even if we are not using it
+// if we wanted too we could cherry pick the features we want to polyfill only
+// this reduces bundle size
+// this is alot of work and normally not done unless you are really worried about bundle size
+
+// this is how to target one method
+import 'core-js/stable/array/filter';
+
+// there is one feature core-js doesn't cover so we need a 2nd package
+// to install "npm i regenerator-runtime"
+
+// for polyfilling async functions
+import 'regenerator-runtime/runtime';
+
+// imports normally go at the top of the file, but they get hoisted to the top anyways
